5fb291ffa3e915606872fbc9a8173c33b8b5b424
coding-to-music <connors.tom@gmail.com>
2022-11-17 20:51:18 +0000

    Build

--- pages/add.js
+++ pages/add.js
@@ -1,23 +1,21 @@
-import Form from 'react-bootstrap/Form'
-import styles from '@/styles/Add.module.scss'
+import Form from "react-bootstrap/Form";
+import styles from "@/styles/Add.module.scss";
 import Spacer from "@/components/Spacer";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import CustomFileInput from "@/components/CustomFileInput";
 import CustomTextArea from "@/components/CustomTextArea";
-import {AddCodeSnippetToDb, UploadCodeImageToS3} from "@/api/codeSnippet";
-import {ShowErrorMessage, ShowSuccessMessage} from "@/utils/messageBox";
+import { AddCodeSnippetToDb, UploadCodeImageToS3 } from "@/api/codeSnippet";
+import { ShowErrorMessage, ShowSuccessMessage } from "@/utils/messageBox";
 import ActionButton from "@/components/ActionButton";
-import {useRouter} from 'next/router'
-import React, {useEffect, useState} from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import firebase from "firebase";
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import Button from "react-bootstrap/Button";
 
 const uiConfig = {
-  signInFlow: 'popup',
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
+  signInFlow: "popup",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   callbacks: {
     // Avoid redirects after sign-in.
     signInSuccessWithAuthResult: () => false,
@@ -27,31 +25,42 @@ const uiConfig = {
 const codeType = {
   image: 1,
   text: 2,
-}
+};
 
 const add = () => {
-  const router = useRouter()
+  const router = useRouter();
   const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
-  const [isCodeType, setCodeType] = useState(codeType.image)
-  const {register, handleSubmit, reset, formState: {errors, isSubmitting}} = useForm();
+  const [isCodeType, setCodeType] = useState(codeType.image);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm();
 
   useEffect(() => {
-    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        const allowedEmails = process.env.NEXT_PUBLIC_ALLOW_EMAILS.split(',')
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        if (user) {
+          const allowedEmails = process.env.NEXT_PUBLIC_ALLOW_EMAILS.split(",");
 
-        if (allowedEmails.includes(user.email)) {
-          setIsSignedIn(true);
-        } else {
-          setIsSignedIn(false);
-          ShowErrorMessage("You cannot access this page. You must add your email. Sorry.")
-          firebase.auth().signOut()
-          setTimeout(() => {
-            router.reload()
-          }, 2000)
+          console.log("add.js useEffect: allowedEmails", allowedEmails);
+
+          if (allowedEmails.includes(user.email)) {
+            setIsSignedIn(true);
+          } else {
+            setIsSignedIn(false);
+            ShowErrorMessage(
+              "You cannot access this page. You must add your email. Sorry."
+            );
+            firebase.auth().signOut();
+            setTimeout(() => {
+              router.reload();
+            }, 2000);
+          }
         }
-      }
-    });
+      });
     return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
   }, []);
 
@@ -60,40 +69,48 @@ const add = () => {
       <div>
         <h1>Personal Code Search</h1>
         <p>In order to add questions/solutions on your own, Please sign-in:</p>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
       </div>
     );
   }
 
-  const onSubmit = async data => {
+  const onSubmit = async (data) => {
+    console.log("add.js onSubmit: data", data);
+
     const postData = {
       ...data,
-    }
+    };
 
     if (!data.rawText || data.rawText === "") {
-      const imageS3Url = await UploadCodeImageToS3(data.image[0])
+      const imageS3Url = await UploadCodeImageToS3(data.image[0]);
       if (imageS3Url === "") {
-        ShowErrorMessage('Error occuring during upload the image to s3.')
-        return
+        ShowErrorMessage("Error occuring during upload the image to s3.");
+        return;
       }
-      postData.imageUrl = imageS3Url
+      postData.imageUrl = imageS3Url;
     }
-    console.log(postData)
-    const success = await AddCodeSnippetToDb(postData)
+    console.log(postData);
+    const success = await AddCodeSnippetToDb(postData);
     if (success) {
-      ShowSuccessMessage('Code snippet has been added successfully. 👊')
-      reset()
-      await router.replace('/')
+      ShowSuccessMessage("Code snippet has been added successfully. 👊");
+      reset();
+      await router.replace("/");
     } else {
-      ShowErrorMessage('Error occuring when trying to attempt add the code snippet to database. 😭')
+      ShowErrorMessage(
+        "Error occuring when trying to attempt add the code snippet to database. 😭"
+      );
     }
   };
 
   return (
-    <Form className={styles.formContainer}
-          onReset={reset}
-          onSubmit={handleSubmit(onSubmit)}>
-
+    <Form
+      className={styles.formContainer}
+      onReset={reset}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <h3 className="text-center my-2">Add Code Snippet</h3>
 
       <Form.Check
@@ -114,25 +131,29 @@ const add = () => {
         onChange={() => setCodeType(codeType.text)}
       />
 
-      {isCodeType === codeType.image && <CustomFileInput
-        controlId="snippetImageControl"
-        title="Snippet Image"
-        inputCssClass={styles.fileGroup}
-        errorMsg="Snippet Image is required."
-        isInputInvalid={errors.image}
-        reactFormProps={register("image")}
-      />}
+      {isCodeType === codeType.image && (
+        <CustomFileInput
+          controlId="snippetImageControl"
+          title="Snippet Image"
+          inputCssClass={styles.fileGroup}
+          errorMsg="Snippet Image is required."
+          isInputInvalid={errors.image}
+          reactFormProps={register("image")}
+        />
+      )}
 
-      {isCodeType === codeType.text && <CustomTextArea
-        controlId="snippetRawTextControl"
-        title="Snippet Raw Text"
-        rows={15}
-        errorMsg="You cannot empty this field"
-        isInputInvalid={errors.rawText}
-        reactFormProps={register("rawText")}
-      />}
+      {isCodeType === codeType.text && (
+        <CustomTextArea
+          controlId="snippetRawTextControl"
+          title="Snippet Raw Text"
+          rows={15}
+          errorMsg="You cannot empty this field"
+          isInputInvalid={errors.rawText}
+          reactFormProps={register("rawText")}
+        />
+      )}
 
-      <Spacer bottomVal={10} topVal={10}/>
+      <Spacer bottomVal={10} topVal={10} />
 
       <CustomTextArea
         controlId="snippetTagControl"
@@ -140,10 +161,10 @@ const add = () => {
         rows={3}
         errorMsg="At least one tag is required"
         isInputInvalid={errors.tag}
-        reactFormProps={register("tag", {required: true})}
+        reactFormProps={register("tag", { required: true })}
       />
 
-      <Spacer bottomVal={10} topVal={10}/>
+      <Spacer bottomVal={10} topVal={10} />
 
       <CustomTextArea
         controlId="snippetTagControl"
@@ -151,28 +172,33 @@ const add = () => {
         rows={3}
         errorMsg="Code description is required"
         isInputInvalid={errors.description}
-        reactFormProps={register("description", {required: true})}
+        reactFormProps={register("description", { required: true })}
       />
 
-      <Spacer bottomVal={10} topVal={10}/>
+      <Spacer bottomVal={10} topVal={10} />
 
       <div className={styles.buttonContainer}>
-        <ActionButton
-          disabledState={isSubmitting}
-          buttonText="Add"
-        />
+        <ActionButton disabledState={isSubmitting} buttonText="Add" />
       </div>
 
       <div className="w-100 mt-3">
         <p className="mb-0">
-          <Button variant="danger"
-                  onClick={() => firebase.auth().signOut().then(() => router.reload())}>Sign-out</Button>
-          {' '}
-          You are signed as {firebase.auth().currentUser.displayName}! If you exit
+          <Button
+            variant="danger"
+            onClick={() =>
+              firebase
+                .auth()
+                .signOut()
+                .then(() => router.reload())
+            }
+          >
+            Sign-out
+          </Button>{" "}
+          You are signed in as {firebase.auth().currentUser.displayName}
         </p>
       </div>
     </Form>
-  )
-}
+  );
+};
 
-export default add
+export default add;
