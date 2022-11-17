2f2e3888d3c7a05f0eeb0c6a8df0c5c52f409aa3
coding-to-music <connors.tom@gmail.com>
2022-11-17 19:57:46 +0000

    Build

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@/styles/globals.scss'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.scss";
 import Layout from "@/components/Layout";
-import '@/utils/firebase'
+import "@/utils/firebase";
+// import ReactGA from 'react-ga;
+
+// export default class MyApp extends App {
+//   componentDidMount() {
+//     ReactGA.initialize(`${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`);
+//     ReactGA.pageview(window.location.pathname + window.location.search);
+//   }
+// }
 
 function MyApp({ Component, pageProps }) {
   return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    )
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
