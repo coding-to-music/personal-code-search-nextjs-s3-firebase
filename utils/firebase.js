391e2ea3f12f44d50edc1ff3cd3a54a2a08b3379
coding-to-music <connors.tom@gmail.com>
2022-11-17 20:33:26 +0000

    Build

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
 let cached = global.firebaseApp;
 if (!cached) {
@@ -10,6 +11,7 @@ function connectFirebase() {
     "firebase.js: NEXT_PUBLIC_FIREBASE_PROJECT_ID",
     process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
   );
+
   return firebase.initializeApp({
     apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}`,
     authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
@@ -19,4 +21,11 @@ function connectFirebase() {
     appId: `${process.env.NEXT_PUBLIC_FIREBASE_APP_ID}`,
     measurementId: `${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`,
   });
+
+  // Initialize Analytics and get a reference to the service
+  // const analytics = getAnalytics(firebase);
+
+  // console.log("firebase.js: analytics", analytics);
+
+  // return firebase;
 }
